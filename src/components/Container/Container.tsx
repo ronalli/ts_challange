0a7b9475d9582db6de705cce975251b772d8b390
Nikita <khakimov.nikita.vch@gmail.com>
2023-05-14 23:08:40 +0300

    first commit

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import styles from './Container.module.scss';
+
+
+interface ContainerProps { 
+	children: ReactNode
+}
+
+export const Container = ({ children }: ContainerProps) =>  (
+		<div className={styles.container} >
+			{children}
+		</div>
+	);
